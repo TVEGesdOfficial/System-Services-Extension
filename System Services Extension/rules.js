7cbfdf05f61b2462f02cad2f268f5d8c91a3756b
TVEGesd <133129617+TVEGesdOfficial@users.noreply.github.com>
2025-03-22 12:45:51 -0500

    Add files via upload

---
+++ System Services Extension/rules.js
@@ -0,0 +1,15 @@
+const blockedSites = [
+    "pornhub.com",
+    "jerkmate.com",
+    "youporn.com",
+    "xvideos.com",
+    'redtube.com',
+    'xhamster.com',
+    'xnxx.com',
+    'turbovideos.com',
+    'tube8.com',
+    'pornpics.com',
+    'hentaihaven.com'
+];
+
+export default blockedSites;
